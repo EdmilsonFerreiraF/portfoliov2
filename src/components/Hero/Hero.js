e29c85c3075cfa7273b56a3d63b335180085a13d
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-03 18:43:21 -0300

    🚀 :rocket: [Add] Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,23 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My purpose is to make difference by coding through technology
+      </SectionText>
+      <Button onClick={
+        () => window.location = 'https://www.linkedin.com/in/edmilson-filho'
+      }>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
