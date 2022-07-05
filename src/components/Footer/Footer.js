d02d9545bc347fe65d7d1d0eda84c77e1fe2bc91
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-05 11:38:36 -0300

    🚀 :rocket: [Add] Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,42 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>
+            Chamar
+          </LinkTitle>
+          <LinkItem href="tel:111-111-1111">
+            111-111-1111
+          </LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>
+            Enviar
+          </LinkTitle>
+          <LinkItem href="mailto:edmilsonf.o3@gmail.com">
+            edmilsonf.o3@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+            Inovando um projeto por vez
+          </Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/edmilsonferreiraf">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com/in/edmilson-filho">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://instagram.com">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
